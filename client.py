8a0db779e4b71f046069e3454a3dd159a2ff5de3
채승지 <cotmdwl1405@gmail.com>
2024-10-12 20:20:34 +0900

    feat : 클라이언트 파일 통신 개발

---
+++ client.py
@@ -0,0 +1,22 @@
+import os
+import time
+import copy
+import random
+import math
+import os
+import time
+import numpy as np
+import pandas as pd
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.utils.data import TensorDataset, DataLoader, Subset
+
+from torchvision import datasets
+from torchvision import transforms
+
+import matplotlib.pyplot as plt
+from PIL import Image
+
+import IPython
