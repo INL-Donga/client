d021d61260354f94211953cf382c732dd15c4c7b
채승지 <cotmdwl1405@gmail.com>
2024-11-28 19:13:36 +0900

    fix: 데이터 이상하게 불러오는 오류 수정

--- v2/sharding/client.py
+++ v2/sharding/client.py
@@ -1,13 +1,10 @@
-import os
 import socket
-import time
-import copy
+import os
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 from torch.utils.data import DataLoader, Subset
 from torchvision import datasets, transforms
-from datetime import datetime
+import time
 import logging
 
 # logging 설정
@@ -24,11 +21,13 @@ device = 'cuda' if torch.cuda.is_available() else 'cpu'
 # 환경 변수 설정
 SERVER_ADDRESS = os.getenv("SERVER_ADDRESS", "127.0.0.1")
 SERVER_PORT = int(os.getenv("SERVER_PORT", 9090))
-EPOCHS = int(os.getenv('EPOCHS', 10))
-CLIENT_ID = int(os.getenv('CLIENT_ID', 1))
-NUM_CLIENTS = int(os.getenv('NUM_CLIENTS', 5))
+EPOCHS = int(os.getenv("EPOCHS", 10))
+NUM_CLIENTS = int(os.getenv("NUM_CLIENTS", 3))
+LEARNING_RATE = float(os.getenv("LEARNING_RATE", 0.01))
+MOMENTUM = float(os.getenv("MOMENTUM", 0.9))
 
-# CIFAR-10 데이터셋 로드 및 데이터 샤딩
+
+# CIFAR-10 데이터셋 로드 및 샤딩
 train_dataset_CIFAR10 = datasets.CIFAR10(
     root='data',
     train=True,
@@ -42,105 +41,78 @@ def get_sharded_dataset(dataset, num_clients, client_id):
     shard_size = len(dataset) // num_clients
     start_idx = (client_id - 1) * shard_size
     end_idx = start_idx + shard_size
+    logger.info(f"Client {client_id}: Dataset range [{start_idx}, {end_idx})")
     return Subset(dataset, indices[start_idx:end_idx])
 
 
-train_dataset_sharded = get_sharded_dataset(
-    train_dataset_CIFAR10, NUM_CLIENTS, CLIENT_ID)
-train_loader_sharded = DataLoader(
-    dataset=train_dataset_sharded,
-    batch_size=64,
-    shuffle=True
-)
+# ComplexCNN 정의
+class ComplexCNN(nn.Module):
+    def __init__(self, grayscale):
+        if grayscale:
+            in_dim = 1
+            self.out_size = 7
+        else:
+            in_dim = 3
+            self.out_size = 8
+        super(ComplexCNN, self).__init__()
 
+        self.conv1_r = nn.Conv2d(
+            in_channels=in_dim, out_channels=16, kernel_size=3, padding=1)
+        self.conv1_i = nn.Conv2d(
+            in_channels=in_dim, out_channels=16, kernel_size=3, padding=1)
 
-# ResNet18 정의
-def conv3x3(in_planes, out_planes, stride=1):
-    return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.conv2_r = nn.Conv2d(
+            in_channels=16, out_channels=32, kernel_size=3, padding=1)
+        self.conv2_i = nn.Conv2d(
+            in_channels=16, out_channels=32, kernel_size=3, padding=1)
 
-
-class BasicBlock_18(nn.Module):
-    expansion = 1
-
-    def __init__(self, inplanes, planes, stride=1, downsample=None):
-        super(BasicBlock_18, self).__init__()
-        self.conv_1 = conv3x3(inplanes, planes, stride)
-        self.bn_1 = nn.BatchNorm2d(planes)
-        self.relu = nn.ReLU(inplace=True)
-        self.conv_2 = conv3x3(planes, planes)
-        self.bn_2 = nn.BatchNorm2d(planes)
-        self.downsample = downsample
-        self.stride = stride
-
-    def forward(self, x):
-        residual = x
-        out = self.conv_1(x)
-        out = self.bn_1(out)
-        out = self.relu(out)
-        out = self.conv_2(out)
-        out = self.bn_2(out)
-        if self.downsample is not None:
-            residual = self.downsample(x)
-        out = out + residual
-        out = self.relu(out)
-        return out
-
-
-class ResNet_18(nn.Module):
-    def __init__(self, block, layers, num_classes, grayscale):
-        self.inplanes = 64
-        in_dim = 1 if grayscale else 3
-        super(ResNet_18, self).__init__()
-        self.conv1g = nn.Conv2d(in_dim, 64, kernel_size=7,
-                                stride=2, padding=3, bias=False)
-        self.bng = nn.BatchNorm2d(64)
-        self.relu = nn.ReLU(inplace=True)
-        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
-        self.layer1 = self._make_layer(block, 64, layers[0])
-        self.layer2 = self._make_layer(block, 128, layers[1], stride=2)
-        self.layer3 = self._make_layer(block, 256, layers[2], stride=2)
-        self.layer4 = self._make_layer(block, 512, layers[3], stride=2)
-        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
-        self.fc = nn.Linear(512 * block.expansion, num_classes)
-
-    def _make_layer(self, block, planes, blocks, stride=1):
-        downsample = None
-        if stride != 1 or self.inplanes != planes * block.expansion:
-            downsample = nn.Sequential(
-                nn.Conv2d(self.inplanes, planes * block.expansion,
-                          kernel_size=1, stride=stride, bias=False),
-                nn.BatchNorm2d(planes * block.expansion),
-            )
-        layers = [block(self.inplanes, planes, stride, downsample)]
-        self.inplanes = planes * block.expansion
-        layers.extend([block(self.inplanes, planes) for _ in range(1, blocks)])
-        return nn.Sequential(*layers)
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.fc1_r = nn.Linear(self.out_size*self.out_size*32, 32)
+        self.fc1_i = nn.Linear(self.out_size*self.out_size*32, 32)
+        self.fc2 = nn.Linear(64, 10)
 
     def forward(self, x):
-        x = self.conv1g(x)
-        x = self.bng(x)
-        x = self.relu(x)
-        x = self.maxpool(x)
-        x = self.layer1(x)
-        x = self.layer2(x)
-        x = self.layer3(x)
-        x = self.layer4(x)
-        x = self.avgpool(x)
-        x = torch.flatten(x, 1)
-        logits = self.fc(x)
-        return logits
+        x_real = self.conv1_r(x) - self.conv1_i(x)
+        x_imag = self.conv1_r(x) + self.conv1_i(x)
+
+        x_real = torch.relu(x_real)
+        x_imag = torch.relu(x_imag)
+
+        x_real = self.pool(x_real)
+        x_imag = self.pool(x_imag)
+
+        x_real_2 = self.conv2_r(x_real) - self.conv2_i(x_imag)
+        x_imag_2 = self.conv2_r(x_imag) + self.conv2_i(x_real)
+
+        x_real_2 = torch.relu(x_real_2)
+        x_imag_2 = torch.relu(x_imag_2)
+
+        x_real_2 = self.pool(x_real_2)
+        x_imag_2 = self.pool(x_imag_2)
+
+        x_real_2 = x_real_2.view(-1, self.out_size*self.out_size * 32)
+        x_imag_2 = x_imag_2.view(-1, self.out_size*self.out_size * 32)
+
+        x_real_fc1 = self.fc1_r(x_real_2) - self.fc1_i(x_imag_2)
+        x_imag_fc1 = self.fc1_r(x_imag_2) + self.fc1_i(x_real_2)
+
+        x_real_fc1 = torch.relu(x_real_fc1)
+        x_imag_fc1 = torch.relu(x_imag_fc1)
+
+        combined = torch.cat([x_real_fc1, x_imag_fc1], dim=1)
+
+        output = self.fc2(combined)
+
+        return output
 
 
-def resnet18(num_classes, grayscale):
-    return ResNet_18(BasicBlock_18, [2, 2, 2, 2], num_classes, grayscale)
-
-
-# 클라이언트 클래스 정의
 class Client:
-    def __init__(self, server_address, max_retries=5, retry_interval=5):
+    def __init__(self, server_address=(SERVER_ADDRESS, SERVER_PORT), max_retries=5, retry_interval=5):
         self.server_address = server_address
         self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.connect_with_retries(max_retries, retry_interval)
+        self.client_id = None  # 클라이언트 ID 초기화
+        self.train_loader_sharded = None  # 샤딩된 데이터 로더 초기화
 
     def connect_with_retries(self, max_retries, retry_interval):
         retries = 0
@@ -158,14 +130,20 @@ class Client:
             raise ConnectionError(
                 "Max retries exceeded, could not connect to the server.")
 
-    def log_message(self, message):
-        logger.info(message)
-
     def get_id(self):
         data = self.s.recv(1024).decode('utf-8')
         client_id = int(data)
         logger.info(f"Client ID received: {client_id}")
-        return client_id
+        self.client_id = client_id
+
+        # 클라이언트 ID로 데이터셋 샤딩 설정
+        train_dataset_sharded = get_sharded_dataset(
+            train_dataset_CIFAR10, NUM_CLIENTS, self.client_id)
+        self.train_loader_sharded = DataLoader(
+            dataset=train_dataset_sharded,
+            batch_size=64,
+            shuffle=True
+        )
 
     def recv_file(self, file_name):
         msg = self.s.recv(1024).decode('utf-8')
@@ -198,32 +176,74 @@ class Client:
                     data = f.read(4096)
             logger.info(f"File {file_name} sent successfully.")
 
+    def get_layer_parameters(self, model, round):
+        if round % 2 == 0:
+            train_weight = {name: param for name, param in model.state_dict().items(
+            ) if 'conv1_i' in name or 'conv2_i' in name or 'fc1_i' in name or 'fc2' in name}
+        else:
+            train_weight = {name: param for name, param in model.state_dict().items(
+            ) if 'conv1_r' in name or 'conv2_r' in name or 'fc1_r' in name or 'fc2' in name}
+
+        return train_weight
+
     def learn(self, client_id, round):
         logger.info("Starting learning phase.")
-        model = resnet18(10, False)
-        model = torch.load('./global_model.pt')
-        model.to(device)
-        model.train()
-        optimizer = torch.optim.SGD(model.parameters(), lr=0.001)
+        local_model = ComplexCNN(False)
+        local_model.load_state_dict(torch.load('./global_model.pt'))
+        local_model.to(device)
+        local_model.train()
+        optimizer = torch.optim.SGD(
+            local_model.parameters(), lr=LEARNING_RATE, momentum=MOMENTUM)
+
+        if round % 2 == 0:
+            for param in local_model.conv1_r.parameters():
+                param.requires_grad = False
+
+            for param in local_model.conv2_r.parameters():
+                param.requires_grad = False
+
+            for param in local_model.fc1_r.parameters():
+                param.requires_grad = False
+        else:
+            for param in local_model.conv1_i.parameters():
+                param.requires_grad = False
+
+            for param in local_model.conv2_i.parameters():
+                param.requires_grad = False
+
+            for param in local_model.fc1_i.parameters():
+                param.requires_grad = False
+
+        total_loss = 0.0
+        total_correct = 0
+        total_samples = 0
 
         for epoch in range(EPOCHS):
-            total_loss, total_correct, total_samples = 0, 0, 0
-            for i, (x, y) in enumerate(train_loader_sharded):
-                x, y = x.to(device), y.to(device)
+            for i, (x, y) in enumerate(self.train_loader_sharded):
+                x = x.to(device)
+                y = y.to(device)
                 optimizer.zero_grad()
-                outputs = model(x)
-                loss = nn.CrossEntropyLoss()(outputs, y)
+                local_out = local_model(x)
+                loss = nn.CrossEntropyLoss()(local_out, y)
                 loss.backward()
                 optimizer.step()
+
+                # 미니배치별 손실 출력
+                # print(
+                #     f"[Round {round} - Epoch {epoch + 1} - Batch {i + 1}] Loss: {loss.item():.4f}")
+
                 total_loss += loss.item() * x.size(0)
-                total_correct += (outputs.argmax(1) == y).sum().item()
+                total_correct += (local_out.argmax(1) == y).sum().item()
                 total_samples += x.size(0)
+
+            # 에포크 종료 시 평균 손실 및 정확도 계산 및 출력
             avg_loss = total_loss / total_samples
             accuracy = total_correct / total_samples
             logger.info(
-                f"Round {round}, Epoch {epoch + 1}/{EPOCHS} - Loss: {avg_loss:.4f}, Accuracy: {accuracy:.4f}")
+                f'Round {round} - Avg Loss: {avg_loss:.4f}, Accuracy: {accuracy:.4f}')
 
-        torch.save(model, f'client_model_{client_id}.pt')
+        model_parameters = self.get_layer_parameters(local_model, round)
+        torch.save(model_parameters, f'client_model_{client_id}.pt')
         logger.info("Learning phase completed.")
 
     def get_updated_model(self, client_id, round):
@@ -231,7 +251,7 @@ class Client:
         if msg == "end":
             return "end"
         self.learn(client_id, round)
-        self.send_file(f"client_model_{client_id}.pt")
+        self.send_file(f'client_model_{client_id}.pt')
         self.s.sendall("done learning\n".encode('utf-8'))
 
     def run(self):
@@ -239,15 +259,15 @@ class Client:
         client_id = self.get_id()
         round = 1
         while True:
-            logger.info(f"Round {round} started.")
+            logger.info(f"Starting round {round}")
             status = self.get_updated_model(client_id, round)
             round += 1
             if status == "end":
-                logger.info("Received termination signal. Exiting.")
+                logger.info("Termination signal received. Exiting.")
                 self.s.sendall("end".encode('utf-8'))
                 break
 
 
 if __name__ == "__main__":
-    client = Client(server_address=(SERVER_ADDRESS, SERVER_PORT))
+    client = Client()
     client.run()
